ed7a822fef1518178853693066eeda70242b9bc9
lucas.azevedo <lucas.verdine@gmail.com>
2024-01-23 23:02:37 -0300

    setup name in state to be uppercase mandatory

--- src/module/validator/state/index.ts
+++ src/module/validator/state/index.ts
@@ -1,7 +1,19 @@
 import * as Joi from 'joi';
 
 export const createStateValidation = Joi.object({
-  name: Joi.string().min(2).max(2).required().messages({
-    'string.pattern.base': `name is a string.`,
-  }),
+  name: Joi.string()
+    .min(2)
+    .max(2)
+    .custom((value, helpers) => {
+      if (value !== value.toUpperCase()) {
+        return helpers.error('string.uppercase', { value });
+      }
+      return value;
+    })
+    .required()
+    .messages({
+      'string.base': 'name must be a string.',
+      'string.length': 'name must have a length of 2.',
+      'any.required': 'name is required.',
+    }),
 });
